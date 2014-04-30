dfc0dd5d3d6adcaf8eeb465cba58d7e63038997f
Scotty Logan <swl@stanford.edu>
2014-04-29 17:16:09 -0700

    added root group and members

--- modules/user/manifests/itlab-users.pp
+++ modules/user/manifests/itlab-users.pp
@@ -5,4 +5,14 @@ class user::itlab-users inherits user::virtual {
     User['swl'],
     User['bvincent'],
   )
+  
+  # ensure that there is a root group with gid 0
+  
+  group { 'root':
+    ensure => present,
+    gid    => 0,
+  }
+  
+  User['swl']      { groups => [ 'root' ] }
+  User['bvincent'] { groups => [ 'root' ] }
 }
