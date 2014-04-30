0b3ba2fd4429278c38e3945a12e9a58cc73867b9
Scotty Logan <swl@stanford.edu>
2014-04-29 17:15:49 -0700

    cleanup; added managehome to non-default users

--- modules/user/manifests/virtual.pp
+++ modules/user/manifests/virtual.pp
@@ -2,40 +2,45 @@
 
 class user::virtual {
 
+  # works with systems where group 1000 has sudo access to 
+  # run anything as root without a password
+  
   @user { 'swl':
-    ensure  => 'present',
-    uid     => '9656',
-    gid     => '0',
-    comment => 'Scotty Logan',
-    home    => '/home/swl',
-    shell   => '/bin/bash',
+    ensure     => 'present',
+    uid        => '9656',
+    gid        => '1000',
+    comment    => 'Scotty Logan',
+    home       => '/home/swl',
+    managehome => true,
+    shell      => '/bin/bash',
   }
 
   @user { 'bvincent':
-    ensure  => 'present',
-    uid     => '18247',
-    gid     => '0',
-    comment => 'Bruce Vincent',
-    home    => '/home/bvincent',
-    shell   => '/bin/bash',
+    ensure     => 'present',
+    uid        => '18247',
+    gid        => '1000',
+    comment    => 'Bruce Vincent',
+    home       => '/home/bvincent',
+    managehome => true,
+    shell      => '/bin/bash',
   }
 
   @user { 'admin':
-    ensure  => 'present',
-    uid     => '1000',
-    gid     => '1000',
-    comment => 'AWS Admin',
-    home    => '/home/admin',
-    shell   => '/bin/bash',
+    ensure     => 'present',
+    uid        => '1000',
+    gid        => '1000',
+    comment    => 'AWS Admin',
+    home       => '/home/admin',
+    shell      => '/bin/bash',
   }
 
   @user { 'vagrant':
-    ensure  => 'present',
-    uid     => '1000',
-    gid     => '1000',
-    comment => 'Vagrant Admin',
-    home    => '/home/vagrant',
-    shell   => '/bin/bash',
+    ensure     => 'present',
+    uid        => '1000',
+    gid        => '1000',
+    comment    => 'Vagrant Admin',
+    home       => '/home/vagrant',
+    shell      => '/bin/bash',
   }
 
-
+}
